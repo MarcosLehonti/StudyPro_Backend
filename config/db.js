38719f5ba4195a9ac7566a1f8be64d527951352b
Marcos-Lehonti <marcoslehontiguzmanmontalvan@gmail.com>
2025-11-20 08:44:12 -0400

    bd

--- config/db.js
+++ config/db.js
@@ -1,13 +1,31 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,      // nombre de la BD
-  process.env.DB_USER,      // usuario
-  process.env.DB_PASSWORD,  // contraseña
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',    // o 'mysql' según tu BD
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,      // nombre de la BD
+//   process.env.DB_USER,      // usuario
+//   process.env.DB_PASSWORD,  // contraseña
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',    // o 'mysql' según tu BD
+//   }
+// );
+
+// module.exports = sequelize;
+
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+// Conexión usando DATABASE_URL de Neon
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Necesario para Neon
+    },
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
+
